39f995de197a48d5183a67d5d5dc39bc1220ff46
Vivek Ghate <ghatevivek77@gmail.com>
2023-12-11 17:30:59 +0530

    Initial commit

---
+++ lib/helper/pdf_helper.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart';
+import 'package:share/share.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PdfHelper {
+
+  static Future<File> saveDocument({
+    required String name,
+    required Document pdf,
+  }) async {
+    final bytes = await pdf.save();
+
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/$name');
+
+    await file.writeAsBytes(bytes);
+
+    return file;
+  }
+
+  static Future openFile(File file) async {
+    final url = file.path;
+
+    await OpenFile.open(url);
+  }
+
+
+
+
+}
+
